c3115c20bdd5e3c27a1c05749d8de4b25fb4ebff
pgrstephen1 <74113003+pgrstephen1@users.noreply.github.com>
2023-01-30 12:25:36 +0800

    Add files via upload

---
+++ project 125.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+from classifier import  get_prediction
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
+
+
+X = np.load('image.npz')['arr_0']
+
+y = pd.read_csv("labels.csv")['labels']
+
+print(pd.Series(y).value_counts())
+
+classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', ;'Y', 'Z']
+
+nclasses = len(classes)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 9, train_size=3500, test_size=500)
+
+def get_prediction(image):
+im-pil = Image.open(image)
+image_bw = im_pil.convert('L')
+image_bw_resized = image_bw.resize((22, 30), Image.ANTIALIAS)
+pixel_filter = 20
+min_pixel = np.percentile(image_bw_resized, pixel_filter)
+image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
+max_pixel = np.max(image_bw_resized)
+image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
+test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,660)
+test_pred = clf.predict(test_sample)
+
+return test_pred[0]
+
+@app.route("/predict-alphabet", methods=["POST"])
+def predict_data():
+  image = request.files.get("alphabet")
+  prediction = get_prediction(image)
+  return jsonify({
+    "prediction": prediction
+  }), 200
+
+if __name__ == "__main__":
+  app.run(debug=True)
